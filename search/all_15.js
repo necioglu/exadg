047d2b38c9d3d35c843a26268a86d9f8f0e9cb3e
necioglu <necioglu@users.noreply.github.com>
2023-05-03 17:55:07 +0000

    deploy: 11a750804e212921a1a449642800f851d7fe3a02

--- search/all_15.js
+++ search/all_15.js
@@ -2,9 +2,9 @@ var searchData=
 [
   ['variablecoefficients_0',['VariableCoefficients',['../classExaDG_1_1VariableCoefficients.html',1,'ExaDG']]],
   ['variablecoefficients_3c_20dealii_3a_3avectorizedarray_3c_20number_20_3e_20_3e_1',['VariableCoefficients&lt; dealii::VectorizedArray&lt; Number &gt; &gt;',['../classExaDG_1_1VariableCoefficients.html',1,'ExaDG']]],
-  ['variablecoefficientscells_2',['VariableCoefficientsCells',['../classExaDG_1_1VariableCoefficientsCells.html',1,'ExaDG']]],
-  ['variablecoefficientscells_3c_20dealii_3a_3avectorizedarray_3c_20number_20_3e_20_3e_3',['VariableCoefficientsCells&lt; dealii::VectorizedArray&lt; Number &gt; &gt;',['../classExaDG_1_1VariableCoefficientsCells.html',1,'ExaDG']]],
-  ['velocityprojection_4',['VelocityProjection',['../classExaDG_1_1VelocityProjection.html',1,'ExaDG']]],
+  ['velocityprojection_2',['VelocityProjection',['../classExaDG_1_1VelocityProjection.html',1,'ExaDG']]],
+  ['viscositymodelbase_3',['ViscosityModelBase',['../classExaDG_1_1IncNS_1_1ViscosityModelBase.html',1,'ExaDG::IncNS']]],
+  ['viscositymodelbase_3c_20dim_2c_20double_20_3e_4',['ViscosityModelBase&lt; dim, double &gt;',['../classExaDG_1_1IncNS_1_1ViscosityModelBase.html',1,'ExaDG::IncNS']]],
   ['viscouskernel_5',['ViscousKernel',['../classExaDG_1_1IncNS_1_1Operators_1_1ViscousKernel.html',1,'ExaDG::IncNS::Operators']]],
   ['viscouskerneldata_6',['ViscousKernelData',['../structExaDG_1_1IncNS_1_1Operators_1_1ViscousKernelData.html',1,'ExaDG::IncNS::Operators']]],
   ['viscousoperator_7',['ViscousOperator',['../classExaDG_1_1CompNS_1_1ViscousOperator.html',1,'ExaDG::CompNS::ViscousOperator&lt; dim, Number &gt;'],['../classExaDG_1_1IncNS_1_1ViscousOperator.html',1,'ExaDG::IncNS::ViscousOperator&lt; dim, Number &gt;']]],
